c6263fcbbcf70aade61f2f7b3ef939f65e24cb9f
Vasudha Venkatesan <M1047097@mindtree.com>
2020-02-17 20:54:23 +0530

    Test changes

--- src/test/java/com/example/test/constructorinjection/TestCakeClassConstructorInjection.java
+++ src/test/java/com/example/test/constructorinjection/TestCakeClassConstructorInjection.java
@@ -3,14 +3,12 @@ package com.example.test.constructorinjection;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.example.ExampleApplicationCI;
 import com.example.constructorinjection.Cake;
-import com.example.dependency.Flavor;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = ExampleApplicationCI.class)
@@ -18,9 +16,6 @@ public class TestCakeClassConstructorInjection {
 
 	@Autowired
 	Cake cake;
-	
-	@Mock
-	Flavor flavor;
 	
 	@Test
 	public void testConstructorInjection() {	
