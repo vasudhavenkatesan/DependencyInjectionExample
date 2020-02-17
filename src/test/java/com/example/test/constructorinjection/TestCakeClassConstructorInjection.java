c35efa4682ff9472c8e27292e5ecfa1b7f056cad
Vasudha Venkatesan <M1047097@mindtree.com>
2020-02-17 14:34:29 +0530

    Initial commit

---
+++ src/test/java/com/example/test/constructorinjection/TestCakeClassConstructorInjection.java
@@ -0,0 +1,30 @@
+package com.example.test.constructorinjection;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.ExampleApplicationCI;
+import com.example.constructorinjection.Cake;
+import com.example.dependency.Flavor;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ExampleApplicationCI.class)
+public class TestCakeClassConstructorInjection {
+
+	@Autowired
+	Cake cake;
+	
+	@Mock
+	Flavor flavor;
+	
+	@Test
+	public void testConstructorInjection() {	
+		String testColor=cake.getFlavor().getColor();
+		Assert.assertEquals(testColor, " White ");
+	}
+}
